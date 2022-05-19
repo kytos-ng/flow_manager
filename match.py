7593509bc2c65a276f8c05f864f255e449e4bb73
Vinicius Arcanjo <vindasil@fiu.edu>
2022-05-19 12:45:02 -0300

    Removed match13_strict because new match_id is being used instead

--- match.py
+++ match.py
@@ -1,7 +1,7 @@
 """Switch match."""
 
 from napps.kytos.flow_manager.v0x01.match import match10_no_strict
-from napps.kytos.flow_manager.v0x04.match import match13_no_strict, match13_strict
+from napps.kytos.flow_manager.v0x04.match import match13_no_strict
 
 
 def match_flow(flow_to_install, version, stored_flow_dict):
@@ -16,14 +16,3 @@ def match_flow(flow_to_install, version, stored_flow_dict):
     elif version == 0x04:
         return match13_no_strict(flow_to_install, stored_flow_dict)
     raise NotImplementedError(f"Unsupported OpenFlow version {version}")
-
-
-def match_strict_flow(flow_to_install, version, stored_flow_dict) -> None:
-    """Match the flow strictly.
-
-    It has support for only for (OF 1.3) flows.
-    If all fields match, return the flow, otherwise return False.
-    """
-    if version != 0x04:
-        raise NotImplementedError(f"Unsupported OpenFlow version {version}")
-    return match13_strict(flow_to_install, stored_flow_dict)
