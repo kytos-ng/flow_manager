9b7ae945d720d2675b5d08ade2b2bd4e8104fe06
David Ramirez <davramir@fiu.edu>
2024-07-22 12:39:49 -0400

    Added default pace for of_lldp flowmods

--- settings.py
+++ settings.py
@@ -42,4 +42,8 @@ ACTION_PACES = {
         "pace": "100/second",
         "strategy": "fixed_window",
     },
+    "send_flow_mod.of_lldp": {
+        "pace": "100/second",
+        "strategy": "fixed_window",
+    },
 }
