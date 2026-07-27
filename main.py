e59ca3b8457af881d27417868f8df35997fc6cc5
quadflow <quadflow@proton.me>
2026-07-27 12:29:49 -0300

    fix: prune _flow_mods_sent dict after using the xid to cut memory usage

--- main.py
+++ main.py
@@ -211,7 +211,7 @@ class Main(KytosNApp):
         with self._flow_mods_sent_lock:
             for flow_xid in flow_xids:
                 try:
-                    flow, cmd, _ = self._flow_mods_sent[flow_xid]
+                    flow, cmd, _ = self._flow_mods_sent.pop(flow_xid)
                 except KeyError:
                     length = len(self._flow_mods_sent)
                     log.error(
@@ -220,7 +220,6 @@ class Main(KytosNApp):
                     continue
                 if (
                     cmd != "add"
-                    or flow_xid not in self._flow_mods_sent
                     or flow_xid in self._flow_mods_sent_error
                 ):
                     continue
